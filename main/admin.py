8dcfb64e5d1ca7800580dc66a0b74714bf8bc7bc
DaveenDev <daveendev07@gmail.com>
2023-03-13 06:01:07 +0800

    created product model and public images settings

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Product
 # Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'price')
+
+    class Meta:
+        verbose_name_plural = "Products"
+
+admin.site.register(Product, ProductAdmin)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=150)
+    description =models.TextField()
+    price = models.FloatField()
+    image = models.ImageField(upload_to="products-images")
+
+    def __str__(self):
+        return self.name
